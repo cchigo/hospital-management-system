05d2e55a3258ecc2e0c3319af2a60636a44835b6
Chigozie Asadu <45165481+ChigozieA@users.noreply.github.com>
2019-05-31 06:33:40 +0100

    create new patients

---
+++ js/mp.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+//    adds a patient
+    $('#add-patient').on("click", function () {
+        let name = $('#name').val();
+        let age = $('#age').val();
+        let phone = $('#phone').val();
+        let gender = $('#gender').val();
+        let checkIn = $('#check-in').val();
+        let allergies = $('#allergies').val();
+        let diagnosis = $('#diagnosis').val();
+        let doctorIncharge = $('#dic').val();
+
+    });
+
+
+
+})
