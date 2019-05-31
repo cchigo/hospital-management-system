f806bdfa3e85842e1a0d89a208a6ca75f2779de9
Chigozie Asadu <45165481+ChigozieA@users.noreply.github.com>
2019-05-31 07:02:06 +0100

    create a new patient

---
+++ js/app.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+
+// function getPatientInfo (s, t) {
+	
+//     // create an empty object to store the mapped result of each index in the strings
+//     var obj = {};
+    
+//     // for (var i = 0; i < s.length; i++) {
+    	
+//     //     if (!obj[s[i]]) {   	
+//     //         obj[s[i]] = t[i];
+     
+//     //     } else if (obj[s[i]] !== t[i]) {
+        	
+//     //         return false;
+//     //     }
+//     // }
+//     // return true;
+// };
+ 
+
+// //function to recieve value of inputs.
+
+// function checkInfo () {
+//     let value1 = document.getElementById('input1').value;
+//     let value2 = document.getElementById('input2').value;
+//     let resultValue = document.getElementById('patient-info');
+
+
+// // lets check if patient info is correct
+// if (value1.length !== value2.length){
+//     resultValue.innerHTML ="Oops! The strings have to be equal in length";
+//     return;
+//     } 
+// // //finally, invoke the function to carry out the check
+// //     let finalResult = isomorphic(value1, value2);
+// //     if(finalResult){
+// //         resultValue.innerHTML = "True: The strings are isomorphic";
+// //         return;
+
+// //     } else {
+// //         resultValue.innerHTML = "False: The strings are not isomorphic";
+// //         return;
+// //     }
+
+// }
+
